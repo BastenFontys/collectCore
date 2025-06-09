d7ea3ce99e4d8c82adb0f2056309aa9ed690757d
basten <basten.hendriks@student.fontys.nl>
2025-06-09 15:20:32 +0200

    created TestPricectrendService

---
+++ colectCoreTest/TestPricetrendService.cs
@@ -0,0 +1,25 @@
+﻿using collectCoreBLL.Services;
+using collectCoreDAL.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colectCoreTest
+{
+    class TestPricetrendService
+    {
+        private readonly Mock<IItemRepo> _itemRepoMock;
+        private readonly Mock<IPricetrendRepo> _priceTrendRepoMock;
+        private readonly PriceTrendService _service;
+
+        public TestPricetrendService()
+        {
+            _itemRepoMock = new Mock<IItemRepo>();
+            _priceTrendRepoMock = new Mock<IPricetrendRepo>();
+            _service = new PriceTrendService(_priceTrendRepoMock.Object, _itemRepoMock.Object);
+        }
+    }
+}
