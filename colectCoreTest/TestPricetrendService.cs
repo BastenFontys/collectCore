8c18f3ace8e74960d181aa8f6b41728e144c83b2
basten <basten.hendriks@student.fontys.nl>
2025-06-10 13:50:49 +0200

    created GetPriceTrend_SomeTrendsNull_SkipsNulls

--- colectCoreTest/TestPricetrendService.cs
+++ colectCoreTest/TestPricetrendService.cs
@@ -53,15 +53,17 @@ namespace colectCoreTest
         [Fact]
         public async Task GetPriceTrend_InvalidRange_Uses1MAsDefault()
         {
+            int collectionId = 1;
+
             var items = new List<ItemDTO>
             {
                 new ItemDTO { ItemID = 1 }
             };
 
-            _itemRepoMock.Setup(repo => repo.GetItemsByCollectionID(It.IsAny<int>())).ReturnsAsync(items);
+            _itemRepoMock.Setup(repo => repo.GetItemsByCollectionID(collectionId)).ReturnsAsync(items);
             _priceTrendRepoMock.Setup(repo => repo.GetPriceTrend1M(1)).ReturnsAsync(new List<float> { 1, 1, 1, 1 });
 
-            var result = await _service.GetPriceTrend(1, "invalid");
+            var result = await _service.GetPriceTrend(collectionId, "invalid");
 
             Assert.Equal(4, result.Count);
             foreach (var value in result)
@@ -74,9 +76,11 @@ namespace colectCoreTest
         [Fact]
         public async Task GetPriceTrend_NoItems_ReturnsZeroList()
         {
-            _itemRepoMock.Setup(repo => repo.GetItemsByCollectionID(It.IsAny<int>())).ReturnsAsync(new List<ItemDTO>());
+            int collectionId = 1;
 
-            var result = await _service.GetPriceTrend(123, "1Y");
+            _itemRepoMock.Setup(repo => repo.GetItemsByCollectionID(collectionId)).ReturnsAsync(new List<ItemDTO>());
+
+            var result = await _service.GetPriceTrend(collectionId, "1Y");
 
             Assert.Equal(12, result.Count);
             foreach (var value in result)
@@ -85,5 +89,30 @@ namespace colectCoreTest
             }
         }
 
+        //De onderstaande test is om te testen wat mijn service class doet als sommige items geen pricetrend heeft
+        [Fact]
+        public async Task GetPriceTrend_SomeTrendsNull_SkipsNulls()
+        {
+            int collectionId = 1;
+
+            var items = new List<ItemDTO>
+            {
+                new ItemDTO { ItemID = 1 },
+                new ItemDTO { ItemID = 2 }
+            };
+
+            _itemRepoMock.Setup(r => r.GetItemsByCollectionID(collectionId)).ReturnsAsync(items);
+
+            _priceTrendRepoMock.Setup(r => r.GetPriceTrend1Y(1)).ReturnsAsync(new List<float> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
+            _priceTrendRepoMock.Setup(r => r.GetPriceTrend1Y(2)).ReturnsAsync((List<float>?)null);
+
+            
+            var result = await _service.GetPriceTrend(collectionId, "1Y");
+
+            
+            Assert.Equal(12, result.Count);
+            Assert.Equal(new List<float> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, result);
+
+        }
     }
 }
