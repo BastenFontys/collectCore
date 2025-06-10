302788c889d00423d27bb054d6b9742f856235cb
basten <basten.hendriks@student.fontys.nl>
2025-06-10 14:15:41 +0200

    created GetPriceTrend_ShorterTrendData_DoesNotCrash

--- colectCoreTest/TestPricetrendService.cs
+++ colectCoreTest/TestPricetrendService.cs
@@ -106,12 +106,35 @@ namespace colectCoreTest
             _priceTrendRepoMock.Setup(r => r.GetPriceTrend1Y(1)).ReturnsAsync(new List<float> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
             _priceTrendRepoMock.Setup(r => r.GetPriceTrend1Y(2)).ReturnsAsync((List<float>?)null);
 
-            
+
             var result = await _service.GetPriceTrend(collectionId, "1Y");
 
-            
+
             Assert.Equal(12, result.Count);
             Assert.Equal(new List<float> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, result);
+        }
+
+        //De onderstaande test is om te testen wat mijn service class doet als items geen volledige pricetrend hebben
+        [Fact]
+        public async Task GetPriceTrend_ShorterTrendData_DoesNotCrash()
+        {
+            int collectionId = 1;
+
+            var items = new List<ItemDTO>
+            {
+                new ItemDTO { ItemID = 1 },
+                new ItemDTO { ItemID = 2 }
+            };
+
+            _itemRepoMock.Setup(r => r.GetItemsByCollectionID(collectionId)).ReturnsAsync(items);
+
+            _priceTrendRepoMock.Setup(r => r.GetPriceTrend1Y(1)).ReturnsAsync(new List<float> { 1, 2, 3 }); // Shorter than 12
+            _priceTrendRepoMock.Setup(r => r.GetPriceTrend1Y(2)).ReturnsAsync(new List<float> { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+
+            var result = await _service.GetPriceTrend(collectionId, "1Y");
+
+            var expected = new List<float> { 5, 7, 9, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            Assert.Equal(expected, result);
 
         }
     }
